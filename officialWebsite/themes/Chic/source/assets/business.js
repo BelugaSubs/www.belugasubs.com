70f887d6266537a314d3d3d8492c999eaf9a8b6f
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-01-20 10:32:24 -0800

    Update business.js

--- officialWebsite/themes/Chic/source/assets/business.js
+++ officialWebsite/themes/Chic/source/assets/business.js
@@ -1,9 +1,9 @@
 // Business 商务合作案例
 document.addEventListener('DOMContentLoaded', function () {
     var cases = [
-        { name: "翼王", startDate: "2021-01-01", description: "内容翻译出海，帮助视频脚本进行英文的本土化翻译，并交由甲方进行YouTube发布", url: "https://space.bilibili.com/475961", imageUrl: "https://s2.loli.net/2024/01/18/SlBnb6HZJo79pqs.jpg" },
-        { name: "笔吧工作室", startDate: "2021-02-01", description: "内容翻译出海，帮助视频原稿进行英文的本土化翻译，并交由甲方进行YouTube发布", url: "https://space.bilibili.com/367877", imageUrl: "https://s2.loli.net/2024/01/18/f4SWlU7ijQP9qk2.jpg" },
-        { name: "极客湾", startDate: "2021-02-01", description: "内容出海优化润色，帮助视频脚本进行英文的本土化优化与润色翻译，并交由甲方进行YouTube发布", url: "https://space.bilibili.com/25876945/", imageUrl: "https://s2.loli.net/2024/01/18/1Md9PmuOgHnvytx.jpg" },
+        { name: "翼王", startDate: "2020-05", description: "内容翻译出海，帮助视频脚本进行英文的本土化翻译，并交由甲方进行YouTube发布", url: "https://space.bilibili.com/475961", imageUrl: "https://s2.loli.net/2024/01/18/SlBnb6HZJo79pqs.jpg" },
+        { name: "笔吧工作室", startDate: "2021-01", description: "内容翻译出海，帮助视频原稿进行英文的本土化翻译，并交由甲方进行YouTube发布", url: "https://space.bilibili.com/367877", imageUrl: "https://s2.loli.net/2024/01/18/f4SWlU7ijQP9qk2.jpg" },
+        { name: "极客湾", startDate: "2021-02", description: "内容出海优化润色，帮助视频脚本进行英文的本土化优化与润色翻译，并交由甲方进行YouTube发布", url: "https://space.bilibili.com/25876945/", imageUrl: "https://s2.loli.net/2024/01/18/1Md9PmuOgHnvytx.jpg" },
     ];
 
     var container = document.querySelector('#business-cases-example');
