36ce832c1c25f9dcd98b886451ac317ac1a91f67
ZL-Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-02-23 23:00:03 +0000

    Deploying to gh-pages from @ ZL-Asica/belugasubs.com@31ee1671305cc0db0387bc2ab9c8d299dd5049bd 🚀

---
+++ assets/business.js
@@ -0,0 +1,27 @@
+// Business 商务合作案例
+document.addEventListener('DOMContentLoaded', function () {
+    var cases = [
+        { name: "翼王", startDate: "2020-05", description: "提供英文脚本本土化翻译，助力YouTube平台发布。", url: "https://space.bilibili.com/475961", imageUrl: "https://s2.loli.net/2024/01/18/SlBnb6HZJo79pqs.jpg" },
+        { name: "笔吧工作室", startDate: "2021-01", description: "协助视频脚本英文翻译与本土化，确保内容符合国际观众的口味。", url: "https://space.bilibili.com/367877", imageUrl: "https://s2.loli.net/2024/01/18/f4SWlU7ijQP9qk2.jpg" },
+        { name: "极客湾", startDate: "2021-02", description: "润色与优化英文脚本，强化其国际传播效果。", url: "https://space.bilibili.com/25876945/", imageUrl: "https://s2.loli.net/2024/01/18/1Md9PmuOgHnvytx.jpg" },
+    ];
+
+    var container = document.querySelector('#business-cases-example');
+
+    cases.forEach(function (caseItem) {
+        var caseEl = document.createElement('div');
+        caseEl.className = 'case';
+        caseEl.innerHTML = `
+            <a id="special-link" href="${caseItem.url}" target="_blank" title="我们旗下的账号">
+                <img src="${caseItem.imageUrl}" alt="${caseItem.name}" loading="lazy">
+            </a>
+            <div class="case-info">
+                <h4>${caseItem.name}</h4>
+                <p>${caseItem.startDate}</p>
+                <p>${caseItem.description}</p>
+            </div>
+            
+        `;
+        container.appendChild(caseEl);
+    });
+});
---
+++ assets/members.js
@@ -0,0 +1,26 @@
+// 页面加载完成后调用自定义函数
+document.addEventListener('DOMContentLoaded', () => {
+    // 初始化地图并设置中心点和缩放级别
+    var map = L.map('membersMap').setView([30.5728, 104.0668], 2.3);
+
+    // 添加地图图层
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    // 使用 fetch API 获取 CSV 数据
+    // CSV 数据格式：latitude,longitude,info
+    fetch('/assets/members.csv')
+        .then(response => response.text()) // 将响应转换为文本
+        .then(csvString => d3.csvParse(csvString)) // 使用 D3.js 解析 CSV 文本
+        .then(data => {
+            // 遍历解析后的数据
+            data.forEach(member => {
+                // 为每个成员创建地图标记
+                L.marker([member.latitude, member.longitude])
+                    .addTo(map) // 将标记添加到地图
+                    .bindPopup(member.info); // 绑定一个弹出窗口显示信息
+            });
+        });
+});
